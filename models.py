c46ea7cb354819cd039a208ff5b16e816b7cbe55
Elizabeth Zietz <schubert26liszt@gmail.com>
2018-07-23 10:59:27 -0700

    models

---
+++ models.py
@@ -0,0 +1,9 @@
+from google.appengine.ext import ndb
+
+class User(ndb.Model):
+    City = ndb.StringProperty(required=True)
+    Country = ndb.StringProperty(required=True)
+    Availability = ndb.StringProperty(required=True)
+    Time Span = ndb.StringProperty(required=True)
+
+print(User.Availability)
