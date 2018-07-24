086ac27cbbf568440a0eb982648d66507c4e064e
alexis mack <demouser@MacBook-Pro.local>
2018-07-23 17:01:51 -0700

    main

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Authorize(webapp2.RequestHandler):
             #user is logged in
             log_url = users.create_logout_url('/')
             log_message = 'Sign Out'
-        else:
+        if not individual:
             #user is not logged in
             log_url = users.create_login_url('/')
             log_message = 'Sign In'
@@ -46,8 +46,15 @@ class Search(webapp2.RequestHandler):
         template = jinja_environment.get_template('results.html')
         self.response.write(template.render())
 
+class Profiles(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('profiles.html')
+        self.response.write(template.render())
+
+
 app = webapp2.WSGIApplication([
     ('/', Home),
     ('/search', Search),
-    ('/authorize', Authorize)
+    ('/authorize', Authorize),
+    ('/profiles', Profiles)
 ], debug=True)
